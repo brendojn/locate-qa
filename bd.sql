10453809bff92f255ab15d22db56835ef79fcf3a
brendojn <brendojnio@gmail.com>
2021-11-17 16:57:39 -0300

    Atualizando o arquivo do banco

--- bd.sql
+++ bd.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
 -- Servidor:                     127.0.0.1
--- Versão do servidor:           5.7.27-0ubuntu0.18.04.1 - (Ubuntu)
+-- Versão do servidor:           8.0.27-0ubuntu0.20.04.1 - (Ubuntu)
 -- OS do Servidor:               Linux
--- HeidiSQL Versão:              10.2.0.5599
+-- HeidiSQL Versão:              11.3.0.6295
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -10,48 +10,68 @@
 /*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
--- Copiando estrutura do banco de dados para locate_qa
-CREATE DATABASE IF NOT EXISTS `location_qa` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `locate_qa`;
+-- Copiando estrutura do banco de dados para location_qa
+DROP DATABASE IF EXISTS `location_qa`;
+CREATE DATABASE IF NOT EXISTS `location_qa` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `location_qa`;
 
--- Copiando estrutura para tabela locate_qa.environments
+-- Copiando estrutura para tabela location_qa.environments
+DROP TABLE IF EXISTS `environments`;
 CREATE TABLE IF NOT EXISTS `environments` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `fk_user_id` int(11) unsigned DEFAULT NULL,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `fk_user_id` int unsigned DEFAULT NULL,
   `name` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_user_id` (`fk_user_id`),
   CONSTRAINT `fk_user_id` FOREIGN KEY (`fk_user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- Exportação de dados foi desmarcado.
 
--- Copiando estrutura para tabela locate_qa.locations
+-- Copiando estrutura para tabela location_qa.locate
+DROP TABLE IF EXISTS `locate`;
 CREATE TABLE IF NOT EXISTS `locate` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `fk_user_id` int(11) unsigned DEFAULT NULL,
-  `name` int(11) unsigned DEFAULT NULL,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `fk_user_id` int unsigned DEFAULT NULL,
+  `name` varchar(60) DEFAULT NULL,
   `device` char(1) DEFAULT '0',
   `environment` char(1) DEFAULT '0',
   `prevision_date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_user_idd` (`fk_user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
 
 -- Exportação de dados foi desmarcado.
 
--- Copiando estrutura para tabela locate_qa.users
+-- Copiando estrutura para tabela location_qa.logs
+DROP TABLE IF EXISTS `logs`;
+CREATE TABLE IF NOT EXISTS `logs` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `fk_locate_id` int unsigned DEFAULT NULL,
+  `description` varchar(500) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_locate_id` (`fk_locate_id`),
+  CONSTRAINT `fk_locate_id` FOREIGN KEY (`fk_locate_id`) REFERENCES `locate` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela location_qa.users
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
   `user` varchar(200) DEFAULT NULL,
   `password` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
 
 -- Exportação de dados foi desmarcado.
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
