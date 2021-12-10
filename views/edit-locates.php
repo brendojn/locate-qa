5ad4951e3630b84950f5eefb4ee0c47392e5a90a
brendojn <brendojnio@gmail.com>
2021-12-10 18:20:36 -0300

    Corrigindo quebra de datas

--- views/edit-locates.php
+++ views/edit-locates.php
@@ -38,12 +38,16 @@ if (empty($_SESSION['logged'])) {
             <label for="prevision_date">Previsão de término</label>
             <input class="form-control" name="prevision_date" type="text"  id="week">
             <script type="text/javascript">
+                var nowDate = new Date();
+                var today = new Date(nowDate.getFullYear(), nowDate.getMonth(), nowDate.getDate(), nowDate.getHours() + 3);
+
                 $(function() {
                     $('input[name="prevision_date"]').daterangepicker( {
                         singleDatePicker: true,
                         timePicker: true,
                         timePicker24Hour: true,
                         timePickerIncrement: 15,
+                        minDate: today,
                         locale: {
                             format: 'DD/MM/YYYY HH:mm'
                         }
